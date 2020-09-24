44bc671a36f909d3a92dc0fcbe33bffbb222b3dc
İrem Kaya <38073758+iiremkayaa@users.noreply.github.com>
2020-09-24 20:11:27 +0300

    added posting sharings

---
+++ Project.Blog.Web/Models/SharingPostModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Blog.Web.Models
+{
+    public class SharingPostModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int? UserId { get; set; }
+       
+        public int? CategoryId { get; set; }
+        public DateTime SharingDate { get; set; }
+    }
+}
