351d4c154ec6769add9c2c3ca3d9414d9ee2fb05
İrem Kaya <38073758+iiremkayaa@users.noreply.github.com>
2020-09-30 20:28:15 +0300

    fixed login and register

--- Project.Blog.Web/Models/UserLoginModel.cs
+++ Project.Blog.Web/Models/UserLoginModel.cs
@@ -11,7 +11,7 @@ namespace Project.Blog.Web.Models
         [Display(Name="Username:")]
         [Required(ErrorMessage ="Username must not be empty.")]
         public string Username { get; set; }
-        [Display(Name ="Password")]
+        [Display(Name="Password:")]
         [Required(ErrorMessage ="Password must not be empty.")]
         public string Password { get; set; }
         public bool RememberMe { get; set; }
--- Project.Blog.Web/Models/UserRegisterModel.cs
+++ Project.Blog.Web/Models/UserRegisterModel.cs
@@ -6,30 +6,30 @@ using System.Threading.Tasks;
 
 namespace Project.Blog.Web.Models
 {
-    public class UserRegisterModel
+    public class UserRegisterModel 
     {
-        [Display(Name="Username")]
+        [Display(Name="Username:")]
         [Required(ErrorMessage="Username must not be empty.")]
         public string Username { get; set; }
 
-        [Display(Name="Password")]
+        [Display(Name="Password:")]
         [Required(ErrorMessage ="Password must not be empty.")]
         public string Password { get; set; }
 
-        [Display(Name="Password")]
+        [Display(Name="Confirm Password:")]
         [Compare("Password", ErrorMessage = "Passwords do not match.")]
         [Required(ErrorMessage ="Confirm password must not be empty.")]
         public string ConfirmPassword { get; set; }
 
-        [Display(Name="Name")]
+        [Display(Name="Name:")]
         [Required(ErrorMessage = "Name must not be empty.")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Last Name must not be empty.")]
-        [Display(Name="Last name")]
+        [Display(Name="Last name:")]
         public string LastName { get; set; }
 
-        [Display(Name="Email")]
+        [Display(Name="Email:")]
         [Required(ErrorMessage = "Email must not be empty.")]
         public string Email { get; set; }
     }
